987c2f57dd4dd6170b7fbac3e5c22c074571504e
Adrien Cuisse <adrien.cuisse@epitech.eu>
2021-12-05 19:50:50 +0100

    value object interface

---
+++ Domain/ValueObjects/ValueObjectInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MyCleanArchBootstrap\Domain\ValueObjects;
+
+/**
+ * Some value with custom validation logic, to compose our entities with
+ * Value-objects are only comparable from values they wrap
+ */
+interface ValueObjectInterface
+{
+    /**
+     * @param ValueObjectInterface $other - the instance to compare with
+     *
+     * @return bool - true if the $other value object represent the same thing (ie., implements the same interface)
+     *  and has same characteristic properties, false otherwise
+     */
+    public function equals(ValueObjectInterface $other): bool;
+}
